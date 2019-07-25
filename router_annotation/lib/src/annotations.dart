b25954fefe2e0657d0e9e2138a7b7aac2cd03921
v7lin <v7lin@qq.com>
2019-07-25 18:01:28 +0800

    zxx

--- router_annotation/lib/src/annotations.dart
+++ router_annotation/lib/src/annotations.dart
@@ -17,24 +17,37 @@ class Component {
 }
 
 class Field {
-  final bool isNullable;
-
   Field({
     this.isNullable,
+    this.dontEncode = false,
+    this.dontDecode = false,
   });
+
+  final bool isNullable;
+  final bool dontEncode;
+  final bool dontDecode;
 }
 
 class Alias implements Field {
   const Alias({
-    this.name,
+    @required this.name,
     this.isNullable,
-  });
+    this.dontEncode = false,
+    this.dontDecode = false,
+  }) : assert(name != null);
 
   final String name;
   final bool isNullable;
+  final bool dontEncode;
+  final bool dontDecode;
 }
 
 class Ignore implements Field {
-  const Ignore() : isNullable = null;
+  const Ignore()
+      : isNullable = null,
+        dontEncode = true,
+        dontDecode = true;
   final bool isNullable;
+  final bool dontEncode;
+  final bool dontDecode;
 }
