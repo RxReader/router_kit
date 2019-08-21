7115e88061d572649e0dfaee9a040fe607b4315a
v7lin <426581@Love>
2019-08-21 15:36:40 +0800

    资源优化

--- example/test/tools/icons/ios.dart
+++ example/test/tools/icons/ios.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:image/image.dart';
+import 'package:path/path.dart' as path;
 
 class IosIconTemplate {
   IosIconTemplate({
@@ -13,23 +14,31 @@ class IosIconTemplate {
 }
 
 List<IosIconTemplate> iosIcons = <IosIconTemplate>[
-  IosIconTemplate(name: 'Icon-App-20x20@1x', size: 20),
-  IosIconTemplate(name: 'Icon-App-20x20@2x', size: 40),
-  IosIconTemplate(name: 'Icon-App-20x20@3x', size: 60),
-  IosIconTemplate(name: 'Icon-App-29x29@1x', size: 29),
-  IosIconTemplate(name: 'Icon-App-29x29@2x', size: 58),
-  IosIconTemplate(name: 'Icon-App-29x29@3x', size: 87),
-  IosIconTemplate(name: 'Icon-App-40x40@1x', size: 40),
-  IosIconTemplate(name: 'Icon-App-40x40@2x', size: 80),
-  IosIconTemplate(name: 'Icon-App-40x40@3x', size: 120),
-  IosIconTemplate(name: 'Icon-App-60x60@2x', size: 120),
-  IosIconTemplate(name: 'Icon-App-60x60@3x', size: 180),
-  IosIconTemplate(name: 'Icon-App-76x76@1x', size: 76),
-  IosIconTemplate(name: 'Icon-App-76x76@2x', size: 152),
-  IosIconTemplate(name: 'Icon-App-83.5x83.5@2x', size: 167),
-  IosIconTemplate(name: 'Icon-App-1024x1024@1x', size: 1024),
+  IosIconTemplate(name: 'Icon-App-20x20@1x.png', size: 20),
+  IosIconTemplate(name: 'Icon-App-20x20@2x.png', size: 40),
+  IosIconTemplate(name: 'Icon-App-20x20@3x.png', size: 60),
+  IosIconTemplate(name: 'Icon-App-29x29@1x.png', size: 29),
+  IosIconTemplate(name: 'Icon-App-29x29@2x.png', size: 58),
+  IosIconTemplate(name: 'Icon-App-29x29@3x.png', size: 87),
+  IosIconTemplate(name: 'Icon-App-40x40@1x.png', size: 40),
+  IosIconTemplate(name: 'Icon-App-40x40@2x.png', size: 80),
+  IosIconTemplate(name: 'Icon-App-40x40@3x.png', size: 120),
+  IosIconTemplate(name: 'Icon-App-60x60@2x.png', size: 120),
+  IosIconTemplate(name: 'Icon-App-60x60@3x.png', size: 180),
+  IosIconTemplate(name: 'Icon-App-76x76@1x.png', size: 76),
+  IosIconTemplate(name: 'Icon-App-76x76@2x.png', size: 152),
+  IosIconTemplate(name: 'Icon-App-83.5x83.5@2x.png', size: 167),
+  IosIconTemplate(name: 'Icon-App-1024x1024@1x.png', size: 1024),
 ];
 
 void createDefaultIcons(Directory outputDir, Image image) {
-//  print('icon path: ${sourceFile.path}');
+  for (IosIconTemplate iosIcon in iosIcons) {
+    Image src = copyResize(image, width: iosIcon.size, height: iosIcon.size, interpolation: Interpolation.average);
+    File save = File(path.join(outputDir.path, iosIcon.name));
+    if (save.existsSync()) {
+      save.deleteSync(recursive: true);
+    }
+    save.createSync(recursive: true);
+    save.writeAsBytesSync(encodePng(src));
+  }
 }
