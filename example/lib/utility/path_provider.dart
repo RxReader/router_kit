be2b8d5a29d575618f222185a79ea9afe79288c8
v7lin <v7lin@qq.com>
2019-09-27 11:57:41 +0800

    xxx

--- example/lib/utility/path_provider.dart
+++ example/lib/utility/path_provider.dart
@@ -8,16 +8,31 @@ import 'package:path_provider/path_provider.dart';
 class PathProvider {
   PathProvider._();
 
+  // cache
   static const String api = 'api';
   static const String images = 'images';
+
+  // file
   static const String nativeLibrary = 'nativeLibrary';
 
-  static FutureOr<Directory> buildCacheDir({
+  static Future<Directory> buildCacheDir({
     @required String type,
     String subtype,
   }) async {
     Directory temporaryDir = await getTemporaryDirectory();
     Directory destDir = Directory(path.join(temporaryDir.path, type, subtype));
+    if (!destDir.existsSync()) {
+      destDir.createSync(recursive: true);
+    }
+    return destDir;
+  }
+
+  static Future<Directory> buildFileDir({
+    @required String type,
+    String subtype,
+  }) async {
+    Directory fileDir = await getApplicationSupportDirectory();
+    Directory destDir = Directory(path.join(fileDir.path, type, subtype));
     if (!destDir.existsSync()) {
       destDir.createSync(recursive: true);
     }
