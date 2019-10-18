49f8ad9cf3b872915601c032276f402c77e794ca
v7lin <v7lin@qq.com>
2019-10-18 17:21:45 +0800

    xxx

--- example/lib/widgets/refresh_page_list_view.dart
+++ example/lib/widgets/refresh_page_list_view.dart
@@ -8,9 +8,7 @@ typedef SliverTapWidgetBuilder = List<Widget> Function(
 typedef SliverHeaderBuilder = List<Widget> Function(
     BuildContext context, bool isEmpty);
 typedef SliverBodyBuilder<T> = List<Widget> Function(
-    BuildContext context, List<T> data);
-typedef SliverCellBuilder<T> = List<Widget> Function(
-    BuildContext context, List<T> data, int index);
+    BuildContext context, T data);
 typedef SliverFooterBuilder = List<Widget> Function(BuildContext context,
     bool isEnd, bool isPagedError, VoidCallback onPagingRetry);
 
@@ -24,11 +22,11 @@ enum _RefreshPageListViewMode {
   refreshing,
 }
 
-abstract class ListModel<T> extends Model {
+abstract class ObjectModel<T> extends Model {
   _RefreshPageListViewMode _mode = _RefreshPageListViewMode.none;
 
-  Key _listKey = GlobalKey();
-  List<T> _data = <T>[];
+  Key _modelKey = GlobalKey();
+  T _data;
   bool _inited = false;
 
   bool get isInited {
@@ -41,9 +39,9 @@ abstract class ListModel<T> extends Model {
     notifyListeners();
 
     try {
-      List<T> newData = await onInit(retry);
-      _data = newData ?? <T>[];
-      _mode = _data.isEmpty
+      T newData = await onInit(retry);
+      _data = newData;
+      _mode = _emptyLike(_data)
           ? _RefreshPageListViewMode.empty
           : _RefreshPageListViewMode.none;
     } catch (e) {
@@ -51,21 +49,25 @@ abstract class ListModel<T> extends Model {
     }
 
     if (retry) {
-      _listKey = GlobalKey();
+      _modelKey = GlobalKey();
     }
 
     _inited = true;
     notifyListeners();
   }
 
-  Future<List<T>> onInit(bool retry);
+  bool _emptyLike(T data) {
+    return data == null;
+  }
+
+  Future<T> onInit(bool retry);
 
   @protected
-  Key get listKey => _listKey;
+  Key get modelKey => _modelKey;
 
   @protected
-  List<T> get data {
-    return List<T>.unmodifiable(_data);
+  T get data {
+    return _data;
   }
 
   bool get isLoading {
@@ -86,13 +88,67 @@ abstract class ListModel<T> extends Model {
   }
 
   @protected
-  M cast<M extends ListModel<T>>() {
+  M cast<M extends ObjectModel<T>>() {
     return this is M ? this as M : null;
   }
 
-  static M of<T, M extends ListModel<T>>(BuildContext context) {
-    return ScopedModel.of<ListModel<T>>(context) as M;
+  static M of<T, M extends ObjectModel<T>>(BuildContext context) {
+    return ScopedModel.of<ObjectModel<T>>(context) as M;
   }
+}
+
+mixin RefreshableModel<T> on ObjectModel<T> {
+  @protected
+  Future<void> tryRefresh() async {
+    _RefreshPageListViewMode lastMode = _mode;
+    _mode = _RefreshPageListViewMode.refreshing;
+    notifyListeners();
+
+    try {
+      T newData = await onRefresh();
+      _data = newData;
+      _mode = _emptyLike(_data)
+          ? _RefreshPageListViewMode.empty
+          : _RefreshPageListViewMode.none;
+
+      _modelKey = GlobalKey();
+    } catch (e) {
+      if (lastMode == _RefreshPageListViewMode.empty) {
+        _mode = _RefreshPageListViewMode.error;
+        _modelKey = GlobalKey();
+      } else {
+        _mode = _RefreshPageListViewMode.none;
+      }
+    }
+
+    notifyListeners();
+  }
+
+  Future<T> onRefresh();
+
+  bool get isRefreshing {
+    return _mode == _RefreshPageListViewMode.refreshing;
+  }
+
+  @protected
+  bool get isRefreshEnable {
+    return _mode != _RefreshPageListViewMode.loading &&
+        _mode != _RefreshPageListViewMode.error &&
+        _mode != _RefreshPageListViewMode.paging;
+  }
+}
+
+mixin ListModel<T> on ObjectModel<List<T>> {
+  @override
+  bool _emptyLike(List<T> data) {
+    if (data == null || data.isEmpty) {
+      return true;
+    }
+    return super._emptyLike(data);
+  }
+
+  @override
+  List<T> get data => super.data != null ? List.unmodifiable(super.data) : null;
 }
 
 mixin PageableModel<T> on ListModel<T> {
@@ -125,45 +181,95 @@ mixin PageableModel<T> on ListModel<T> {
   }
 }
 
-mixin RefreshableModel<T> on ListModel<T> {
-  @protected
-  Future<void> tryRefresh() async {
-    _mode = _RefreshPageListViewMode.refreshing;
-    notifyListeners();
+class RefreshPageView<T> extends StatefulWidget {
+  const RefreshPageView({
+    Key key,
+    this.scrollKey,
+    @required this.model,
+    @required this.sliverBodyBuilder,
+    SliverWidgetBuilder sliverLoadingBuilder,
+    SliverWidgetBuilder sliverEmptyBuilder,
+    SliverTapWidgetBuilder sliverErrorBuilder,
+    this.displacement = 40.0,
+    this.physics,
+    this.controller,
+    this.cacheExtent = 100.0,
+  })  : sliverLoadingBuilder = sliverLoadingBuilder ?? null,
+        sliverEmptyBuilder = sliverEmptyBuilder ?? null,
+        sliverErrorBuilder = sliverErrorBuilder ?? null,
+        super(key: key);
 
-    try {
-      List<T> newData = await onRefresh();
-      _data = newData ?? <T>[];
-      _mode = _data.isEmpty
-          ? _RefreshPageListViewMode.empty
-          : _RefreshPageListViewMode.none;
-    } catch (e) {
-      _mode = _data.isEmpty
-          ? _RefreshPageListViewMode.error
-          : _RefreshPageListViewMode.none;
+  final Key scrollKey;
+  final ObjectModel<T> model;
+  final SliverBodyBuilder<T> sliverBodyBuilder;
+  final SliverWidgetBuilder sliverLoadingBuilder;
+  final SliverWidgetBuilder sliverEmptyBuilder;
+  final SliverTapWidgetBuilder sliverErrorBuilder;
+  final double displacement;
+  final ScrollPhysics physics;
+  final ScrollController controller;
+  final double cacheExtent;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _RefreshPageViewState<T>();
+  }
+}
+
+class _RefreshPageViewState<T> extends State<RefreshPageView<T>> {
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModel<ObjectModel<T>>(
+      model: widget.model,
+      child: ScopedModelDescendant<ObjectModel<T>>(
+        builder: (BuildContext context, Widget child, ObjectModel<T> model) {
+          RefreshableModel<T> refreshableModel =
+              model.cast<RefreshableModel<T>>();
+          Widget child = Builder(
+            key: model.modelKey, // 刷新后必须清理列表里的 StatefulWidget 的 State
+            builder: (BuildContext context) {
+              return CustomScrollView(
+                key: widget.scrollKey,
+                scrollDirection: Axis.vertical,
+                physics: widget.physics,
+                controller: widget.controller,
+                cacheExtent: widget.cacheExtent,
+                slivers: <Widget>[
+                  ..._buildSliverBody(context, model),
+                ],
+              );
+            },
+          );
+          return refreshableModel != null
+              ? RefreshIndicator(
+                  child: child,
+                  displacement: widget.displacement,
+                  onRefresh: refreshableModel.tryRefresh,
+                )
+              : child;
+        },
+      ),
+    );
+  }
+
+  List<Widget> _buildSliverBody(BuildContext context, ObjectModel<T> model) {
+    if (model.isLoading) {
+      return widget.sliverLoadingBuilder(context);
     }
-
-    _listKey = GlobalKey();
-
-    notifyListeners();
-  }
-
-  Future<List<T>> onRefresh();
-
-  bool get isRefreshing {
-    return _mode == _RefreshPageListViewMode.refreshing;
-  }
-
-  @protected
-  bool get isRefreshEnable {
-    return _mode != _RefreshPageListViewMode.loading &&
-        _mode != _RefreshPageListViewMode.error &&
-        _mode != _RefreshPageListViewMode.paging;
+    if (model.isEmpty) {
+      return widget.sliverEmptyBuilder(context);
+    }
+    if (model.isError) {
+      return widget.sliverErrorBuilder(context, () {
+        model.tryInit(true);
+      });
+    }
+    return widget.sliverBodyBuilder(context, model.data);
   }
 }
 
 class RefreshPageListView<T> extends StatefulWidget {
-  RefreshPageListView.custom({
+  RefreshPageListView({
     Key key,
     this.scrollKey,
     @required this.model,
@@ -178,58 +284,18 @@ class RefreshPageListView<T> extends StatefulWidget {
     this.controller,
     this.cacheExtent = 100.0,
   })  : assert(model != null),
-        sliverFooterBuilder = sliverFooterBuilder ??
-            (model is PageableModel<T> ? null : null),
-        sliverLoadingBuilder =
-            sliverLoadingBuilder ?? null,
+        sliverFooterBuilder =
+            sliverFooterBuilder ?? (model is PageableModel<T> ? null : null),
+        sliverLoadingBuilder = sliverLoadingBuilder ?? null,
         sliverEmptyBuilder = sliverEmptyBuilder ?? null,
         sliverErrorBuilder = sliverErrorBuilder ?? null,
         assert(sliverBodyBuilder != null),
         super(key: key);
 
-  RefreshPageListView.builder({
-    Key key,
-    Key scrollKey,
-    @required ListModel<T> model,
-    SliverHeaderBuilder sliverHeaderBuilder,
-    @required SliverCellBuilder<T> sliverItemBuilder,
-    SliverFooterBuilder sliverFooterBuilder,
-    SliverWidgetBuilder sliverLoadingBuilder,
-    SliverWidgetBuilder sliverEmptyBuilder,
-    SliverTapWidgetBuilder sliverErrorBuilder,
-    double displacement = 40.0,
-    ScrollPhysics physics,
-    ScrollController controller,
-    double cacheExtent = 100.0,
-  }) : this.custom(
-          key: key,
-          scrollKey: scrollKey,
-          model: model,
-          sliverHeaderBuilder: sliverHeaderBuilder,
-          sliverBodyBuilder: (BuildContext context, List<T> data) => data
-              .map((T e) {
-                return sliverItemBuilder(
-                  context,
-                  data,
-                  data.indexOf(e),
-                );
-              })
-              .expand((List<Widget> pairs) => pairs)
-              .toList(),
-          sliverFooterBuilder: sliverFooterBuilder,
-          sliverLoadingBuilder: sliverLoadingBuilder,
-          sliverEmptyBuilder: sliverEmptyBuilder,
-          sliverErrorBuilder: sliverErrorBuilder,
-          displacement: displacement,
-          physics: physics,
-          controller: controller,
-          cacheExtent: cacheExtent,
-        );
-
   final Key scrollKey;
   final ListModel<T> model;
   final SliverHeaderBuilder sliverHeaderBuilder;
-  final SliverBodyBuilder<T> sliverBodyBuilder;
+  final SliverBodyBuilder<List<T>> sliverBodyBuilder;
   final SliverFooterBuilder sliverFooterBuilder;
   final SliverWidgetBuilder sliverLoadingBuilder;
   final SliverWidgetBuilder sliverEmptyBuilder;
@@ -241,11 +307,11 @@ class RefreshPageListView<T> extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() {
-    return RefreshPageListViewState<T>();
+    return _RefreshPageListViewState<T>();
   }
 }
 
-class RefreshPageListViewState<T> extends State<RefreshPageListView<T>> {
+class _RefreshPageListViewState<T> extends State<RefreshPageListView<T>> {
   @override
   void initState() {
     super.initState();
@@ -261,10 +327,10 @@ class RefreshPageListViewState<T> extends State<RefreshPageListView<T>> {
       child: ScopedModelDescendant<ListModel<T>>(
         builder: (BuildContext context, Widget child, ListModel<T> model) {
           PageableModel<T> pageableModel = model.cast<PageableModel<T>>();
-          RefreshableModel<T> refreshableModel =
-              model.cast<RefreshableModel<T>>();
+          RefreshableModel<List<T>> refreshableModel =
+              model.cast<RefreshableModel<List<T>>>();
           Widget child = Builder(
-            key: model.listKey, // 刷新后必须清理列表里的 StatefulWidget 的 State
+            key: model.modelKey, // 刷新后必须清理列表里的 StatefulWidget 的 State
             builder: (BuildContext context) {
               return CustomScrollView(
                 key: widget.scrollKey,
