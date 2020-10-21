b954cf929ef724fb63a1bcf2b33089c64ce9777c
v7lin <v7lin@qq.com>
2020-10-21 14:16:27 +0800

    修正配置

--- example/lib/router/app_router.g.dart
+++ example/lib/router/app_router.g.dart
@@ -7,11 +7,31 @@ part of 'app_router.dart';
 // **************************************************************************
 
 import 'package:flutter/widgets.dart';
+import 'package:example/pages/home/home_page.dart';
+import 'package:example/pages/about/about_page.dart';
+import 'package:example/pages/login/login_page.dart';
+import 'package:example/pages/not_found/not_found_page.dart';
+import 'package:example/pages/params/params_page.dart';
+import 'package:example/pages/payment/payment_page.dart';
 
 class AppRouterProvider {
   const AppRouterProvider._();
 
-  static final Map<String, String> names = <String, String>{};
+  static final Map<String, String> names = <String, String>{
+    HomePageProvider.routeName: HomePageProvider.name,
+    AboutPageProvider.routeName: AboutPageProvider.name,
+    LoginPageProvider.routeName: LoginPageProvider.name,
+    NotFoundPageProvider.routeName: NotFoundPageProvider.name,
+    ParamsPageProvider.routeName: ParamsPageProvider.name,
+    PaymentPageProvider.routeName: PaymentPageProvider.name,
+  };
 
-  static final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{};
+  static final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
+    HomePageProvider.routeName: HomePageProvider.routeBuilder,
+    AboutPageProvider.routeName: AboutPageProvider.routeBuilder,
+    LoginPageProvider.routeName: LoginPageProvider.routeBuilder,
+    NotFoundPageProvider.routeName: NotFoundPageProvider.routeBuilder,
+    ParamsPageProvider.routeName: ParamsPageProvider.routeBuilder,
+    PaymentPageProvider.routeName: PaymentPageProvider.routeBuilder,
+  };
 }
