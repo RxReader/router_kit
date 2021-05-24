1ab57aeb4a7943105c77485702c8089fb042b954
v7lin <v7lin@qq.com>
2021-05-24 19:47:28 +0800

    router_api 0.2.1

--- router_api/CHANGELOG.md
+++ router_api/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.1
+
+* dynamic -> Object
+
 ## 0.2.0
 
 * nullsafety
--- router_api/lib/src/router.dart
+++ router_api/lib/src/router.dart
@@ -2,8 +2,13 @@ import 'dart:async';
 
 import 'package:flutter/widgets.dart' hide Router;
 
-typedef Next = Future<dynamic?> Function();
-typedef Interceptor = Future<dynamic?> Function(BuildContext context, String routeName, {Object? arguments, Next? next});
+typedef Next = Future<Object?> Function();
+typedef Interceptor = Future<Object?> Function(
+  BuildContext context,
+  String routeName, {
+  Object? arguments,
+  Next? next,
+});
 
 abstract class Controller {
   String get name;
@@ -23,13 +28,16 @@ class Router {
   final Map<String, WidgetBuilder> _routes = <String, WidgetBuilder>{};
   final Map<String, Interceptor> _routeInterceptors = <String, Interceptor>{};
 
-  List<Interceptor> get interceptors => List<Interceptor>.unmodifiable(_interceptors);
+  List<Interceptor> get interceptors =>
+      List<Interceptor>.unmodifiable(_interceptors);
 
   Map<String, String> get names => Map<String, String>.unmodifiable(_names);
 
-  Map<String, WidgetBuilder> get routes => Map<String, WidgetBuilder>.unmodifiable(_routes);
+  Map<String, WidgetBuilder> get routes =>
+      Map<String, WidgetBuilder>.unmodifiable(_routes);
 
-  Map<String, Interceptor> get routeInterceptors => Map<String, Interceptor>.unmodifiable(_routeInterceptors);
+  Map<String, Interceptor> get routeInterceptors =>
+      Map<String, Interceptor>.unmodifiable(_routeInterceptors);
 
   @protected
   void registerBuiltIn() {}
@@ -39,8 +47,15 @@ class Router {
     _interceptors.add(interceptor);
   }
 
-  void useRoute({required String name, required String routeName, required WidgetBuilder routeBuilder, Interceptor? interceptor}) {
-    assert(!_names.containsKey(routeName) && !_routes.containsKey(routeName) && !_routeInterceptors.containsKey(routeName));
+  void useRoute({
+    required String name,
+    required String routeName,
+    required WidgetBuilder routeBuilder,
+    Interceptor? interceptor,
+  }) {
+    assert(!_names.containsKey(routeName) &&
+        !_routes.containsKey(routeName) &&
+        !_routeInterceptors.containsKey(routeName));
     _names[routeName] = name;
     _routes[routeName] = routeBuilder;
     if (interceptor != null) {
@@ -48,7 +63,15 @@ class Router {
     }
   }
 
-  void useController({required Map<String, dynamic> controller, Interceptor? interceptor}) {
-    useRoute(name: controller['name'] as String, routeName: controller['routeName'] as String, routeBuilder: controller['routeBuilder'] as WidgetBuilder, interceptor: interceptor);
+  void useController({
+    required Map<String, dynamic> controller,
+    Interceptor? interceptor,
+  }) {
+    useRoute(
+      name: controller['name'] as String,
+      routeName: controller['routeName'] as String,
+      routeBuilder: controller['routeBuilder'] as WidgetBuilder,
+      interceptor: interceptor,
+    );
   }
 }
