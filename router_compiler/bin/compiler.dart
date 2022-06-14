6998a90ccadf3e42398c60c70b971eac2347159d
v7lin <v7lin@qq.com>
2022-06-14 15:08:09 +0800

    Update compiler.dart

--- router_compiler/bin/compiler.dart
+++ router_compiler/bin/compiler.dart
@@ -18,14 +18,42 @@ class BuildCommand extends Command<void> {
   FutureOr<void> run() async {
     log('${runner?.executableName} $name');
     log('clean \u2026');
-    final ProcessResult clean = await Process.run('flutter', <String>['pub', 'run', 'build_runner', 'clean']);
+    final ProcessResult clean = await Process.run(
+      'flutter',
+      <String>['pub', 'run', 'build_runner', 'clean'],
+    );
     if (clean.exitCode != 0) {
-      throw ProcessException('flutter', <String>['pub', 'run', 'build_runner', 'clean'], clean.stderr as String ?? '', clean.exitCode);
+      throw ProcessException(
+        'flutter',
+        <String>['pub', 'run', 'build_runner', 'clean'],
+        clean.stderr as String ?? '',
+        clean.exitCode,
+      );
     }
     log('build provider && manifest \u2026');
-    final ProcessResult build = await Process.run('flutter', <String>['pub', 'run', 'build_runner', 'build', '--delete-conflicting-outputs']);
+    final ProcessResult build = await Process.run(
+      'flutter',
+      <String>[
+        'pub',
+        'run',
+        'build_runner',
+        'build',
+        '--delete-conflicting-outputs'
+      ],
+    );
     if (build.exitCode != 0) {
-      throw ProcessException('flutter', <String>['pub', 'run', 'build_runner', 'build', '--delete-conflicting-outputs'], build.stderr as String ?? '', build.exitCode);
+      throw ProcessException(
+        'flutter',
+        <String>[
+          'pub',
+          'run',
+          'build_runner',
+          'build',
+          '--delete-conflicting-outputs'
+        ],
+        build.stderr as String ?? '',
+        build.exitCode,
+      );
     }
   }
 }
